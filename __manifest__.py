86b9e01cf3d811ad72f8b944bc53768833d2eced
onlyone-odoo <matiasb@onlyone.com.ar>
2025-03-09 19:02:28 -0300

    [FIX][invoice_barcodes][report barcode]

--- __manifest__.py
+++ __manifest__.py
@@ -13,7 +13,7 @@
     "website": "https://onlyone.odoo.com/",
     "license": "AGPL-3",
     "category": "Technical Settings",
-    "version": "17.0.6.3.1",
+    "version": "17.0.6.4.1",
     "development_status": "Production/Stable",
     "application": False,
     "installable": True,
